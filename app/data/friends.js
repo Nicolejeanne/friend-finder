d147784dd7e545f0032d46a6c85f60e17c7edfc3
Nicolejeanne <nicolejeanne@users.noreply.github.com>
2018-10-29 23:53:43 -0500

    added content to initial friends array

---
+++ app/data/friends.js
@@ -0,0 +1,13 @@
+// DATA
+// Below data will hold all of the friends information
+
+let friendsArray = [
+  {
+    name: "Bob Bobby",
+    photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhgq6zNL0YcjFnSP_xDtpBwucFsZR4qcOx52XKu8Y7fiWT__ZQ",
+    scores: [5, 2, 5, 3, 4, 1, 1, 4, 2, 5]
+  }
+];
+
+// Exporting the array. This makes it accessible to other files using require.
+module.exports = friendsArray;
